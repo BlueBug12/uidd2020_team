5aa4270c3f083fd8b77b37f4f08a6b1290001cfd
BlueBug <benson.liu.himself@gmail.com>
2020-04-04 18:37:08 +0800

    add dark masker

--- about_us/About_Us.js
+++ about_us/About_Us.js
@@ -2,6 +2,7 @@ $(function(){
   $('#know-more').on('click',function(){
     $('#topic').transition('fade',300)
     $(this).transition('fade',300)
+    $('#darker-bg').animate({opacity:"0"})
     $('#top-house').animate({bottom:"40%"})
     $('#bottom-house').animate({bottom:"0%"})
     $('#houseworks').css('visibility','visible')
