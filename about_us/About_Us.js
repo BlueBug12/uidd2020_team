7e4fa6ae01b8e30b0f49186667612e5d3821d722
BlueBug <benson.liu.himself@gmail.com>
2020-04-06 18:52:34 +0800

    house part complete

--- about_us/About_Us.js
+++ about_us/About_Us.js
@@ -1,12 +1,10 @@
 $(function(){
   $('#know-more').on('click',function(){
     $('#topic,#know-more').addClass('animated fadeOut')
-//    $('#topic').transition('fade',300)
-  //  $(this).transition('fade',300)
     $('#darker-bg').animate({opacity:"0"}).css("z-index","-1")
-    $('#top-house').animate({bottom:"40%"},1000)
-    $('#bottom-house').animate({bottom:"0%"},1000)
-    $('#base').animate({bottom:"-7%"},1000)
+    $('#top-house').animate({bottom:"60%"},1000)
+    $('#bottom-house').animate({bottom:"40%"},1000)
+    $('#base').animate({bottom:"38%"},1000)
     $('#houseworks').delay(1400).css('visibility','visible')
 		$('#w1').delay(1400).addClass('animated fadeInUp')
 		$('#w2').delay(1400).addClass('animated fadeInDown')
@@ -16,28 +14,19 @@ $(function(){
     $('#sub-section-2').css('visibility','hidden')
 
     $('html, body').delay(2500).animate({ scrollTop: $('#section_2').offset().top}, 500, 'linear',function(){ 
-      setTimeout(function(){
         $('#sub-section-2').addClass('animated bounceInDown').css('visibility','visible')
-      },100);
     })
-    //$('#house').delay(2300).css('visibility','visible')
 		
-		/*
-    $('#section_1_sub').delay(2000).animate({
-      top:"50vh",
-      left:"5vw",
-      width:"40vw",
-      height:"30vh"})*/
   })
 })
-
+/*
 $(function () {
   $(window).scroll(function () {
     var scrollVal = $(this).scrollTop();
     console.log(scrollVal);
   });
 });
-/*
+
 $(function() {
     $('a[href*=\\#]').on('click', function(e) {
       e.preventDefault();
