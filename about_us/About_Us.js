9ad778fd4992a8a9ac57240c2c6e576caef237ba
robertlin0401 <robertlin0401@gmail.com>
2020-04-07 22:17:00 +0800

    Feature: section 1 display fits viewsize

--- about_us/About_Us.js
+++ about_us/About_Us.js
@@ -2,9 +2,9 @@ $(function(){
   $('#know-more').on('click',function(){
     $('#topic,#know-more').addClass('animated fadeOut')
     $('#darker-bg').animate({opacity:"0"}).css("z-index","-1")
-    $('#top-house').animate({bottom:"60%"},1000)
-    $('#bottom-house').animate({bottom:"40%"},1000)
-    $('#base').animate({bottom:"38%"},1000)
+    $('#top-house').css({top: "calc(-10.3vh + 80px)", transition: "1s"})
+    $('#bottom-house').css({top: "calc(-4vh + 80px)", transition: "1s"})
+    $('#base').css({top: "calc(2.5vh + 80px)", transition: "1s"})
     $('#houseworks').delay(1400).css('visibility','visible')
 		$('#w1').delay(1400).addClass('animated fadeInUp')
 		$('#w2').delay(1400).addClass('animated fadeInDown')
@@ -16,22 +16,18 @@ $(function(){
     $('html, body').delay(2500).animate({ scrollTop: $('#section_2').offset().top}, 500, 'linear',function(){ 
         $('#sub-section-2').addClass('animated bounceInDown').css('visibility','visible')
     })
-		
+    $('#know-more').transition('fade');
+
+    $('#top-house').css({transition: "0s"})
+    $('#bottom-house').css({transition: "0s"})
+    $('#base').css({transition: "0s"})
+
   })
 })
 
 $(function() {
-  // var touch = document.body;
-  // var click = 0;
   var state = 0;
   var trash_pos = $('#w4_orange').position();
-  // touch.onclick = function() {
-  //     if (click == 0) {
-  //         document.getElementById('w4_orange').style.visibility = 'hidden';
-  //         click = 1;
-  //         return;
-  //     }
-  // };
 
   const track = document.querySelector('.carousel_track');
   const slides = Array.from(track.children);
@@ -76,8 +72,7 @@ $(function() {
           var y_per = (1 / 2.8) * 100;
           $('#w4_orange').css("left", x_per + "%");
           $('#w4_orange').css("top", y_per + "%");
-          console.log(trash_pos.left / parseInt($(document).width()));
-          console.log(trash_pos.top / parseInt($(document).height()));
+          $('.fadeout').css({"transform": "scale(3.0)", opacity: 0, transition: '3s'});
           state = 2;
           return;
       }
