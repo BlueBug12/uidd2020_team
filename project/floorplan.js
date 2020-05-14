8e9ad6a6af34289fdc51ed0267dfdfc4514009a3
Anderson-Wu <f74079025@gs.ncku.edu.tw>
2020-05-14 14:26:40 +0800

    room-item can be clicked

--- project/floorplan.js
+++ project/floorplan.js
@@ -1,77 +1,92 @@
-var init = '{"floorplan":{"corners":{"e9384d8b-0850-6bf7-eff2-6eda3e59f8f9":{"x":832.129393798828,"y":-202.6919999999999},"2e5b1928-55ba-0655-46d6-5572a01b3600":{"x":204.24139379882803,"y":-202.6919999999999},"ff096a8b-4b15-4016-ed0e-cafa8a52f7a0":{"x":204.24139379882803,"y":-332.7399999999999},"17e15b26-b2eb-0dcd-3084-6e2e4539042b":{"x":-68.04660620117198,"y":-332.7399999999999},"3b7d00c7-3810-db28-1e11-0aa39e2d79e1":{"x":-68.04660620117198,"y":-552.1959999999999},"076e7cbe-02cd-3526-ca1b-eb202ce7e596":{"x":96.545393798828,"y":-552.1959999999999},"2fd80f79-ae1e-2d61-3613-063730bf2b44":{"x":96.545393798828,"y":-814.324},"f2447823-3420-97e4-3933-5f26e7415420":{"x":334.28939379882803,"y":-814.324},"62c1db8e-b885-b644-baa8-0cfcc882f3a1":{"x":334.28939379882803,"y":-592.8359999999999},"d1a44234-d8b1-ab14-330e-b0002b20b8d1":{"x":549.6813937988279,"y":-592.8359999999999},"3fce7aad-0427-f002-dc6e-0f2175286a80":{"x":549.6813937988279,"y":-706.6279999999999},"a650437f-1db1-da3f-fbce-213587a6289e":{"x":832.129393798828,"y":-706.6279999999999}},"walls":[{"corner1":"a650437f-1db1-da3f-fbce-213587a6289e","corner2":"e9384d8b-0850-6bf7-eff2-6eda3e59f8f9","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}},{"corner1":"e9384d8b-0850-6bf7-eff2-6eda3e59f8f9","corner2":"2e5b1928-55ba-0655-46d6-5572a01b3600","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}},{"corner1":"2e5b1928-55ba-0655-46d6-5572a01b3600","corner2":"ff096a8b-4b15-4016-ed0e-cafa8a52f7a0","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}},{"corner1":"ff096a8b-4b15-4016-ed0e-cafa8a52f7a0","corner2":"17e15b26-b2eb-0dcd-3084-6e2e4539042b","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}},{"corner1":"17e15b26-b2eb-0dcd-3084-6e2e4539042b","corner2":"3b7d00c7-3810-db28-1e11-0aa39e2d79e1","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}},{"corner1":"3b7d00c7-3810-db28-1e11-0aa39e2d79e1","corner2":"076e7cbe-02cd-3526-ca1b-eb202ce7e596","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}},{"corner1":"076e7cbe-02cd-3526-ca1b-eb202ce7e596","corner2":"2fd80f79-ae1e-2d61-3613-063730bf2b44","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}},{"corner1":"2fd80f79-ae1e-2d61-3613-063730bf2b44","corner2":"f2447823-3420-97e4-3933-5f26e7415420","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}},{"corner1":"f2447823-3420-97e4-3933-5f26e7415420","corner2":"62c1db8e-b885-b644-baa8-0cfcc882f3a1","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}},{"corner1":"62c1db8e-b885-b644-baa8-0cfcc882f3a1","corner2":"d1a44234-d8b1-ab14-330e-b0002b20b8d1","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}},{"corner1":"d1a44234-d8b1-ab14-330e-b0002b20b8d1","corner2":"3fce7aad-0427-f002-dc6e-0f2175286a80","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}},{"corner1":"3fce7aad-0427-f002-dc6e-0f2175286a80","corner2":"a650437f-1db1-da3f-fbce-213587a6289e","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}}],"wallTextures":[],"floorTextures":{},"newFloorTextures":{}},"items":[{"item_name":"chair","item_type":1,"model_url":"furnish/chair.png","xpos":650,"ypos":230,"rotation":0,"scale_x":1,"scale_y":1,"fixed":false},{"item_name":"sofa","item_type":1,"model_url":"furnish/sofa.png","xpos":660,"ypos":120,"rotation":0,"scale_x":1,"scale_y":1,"fixed":false},{"item_name":"door","item_type":1,"model_url":"furnish/door.png","xpos":200,"ypos":250,"rotation":0,"scale_x":1,"scale_y":1,"fixed":false},{"item_name":"toilet","item_type":1,"model_url":"furnish/toilet.png","xpos":90,"ypos":300,"rotation":0,"scale_x":1,"scale_y":1,"fixed":false},{"item_name":"sink","item_type":1,"model_url":"furnish/sink.png","xpos":90,"ypos":270,"rotation":0,"scale_x":1,"scale_y":1,"fixed":false},{"item_name":"bathtub","item_type":1,"model_url":"furnish/bathtub.png","xpos":90,"ypos":330,"rotation":0,"scale_x":1,"scale_y":1,"fixed":false}],"rooms":[{"id":"room_1","text":"二哥房間","color":"#E0E0FF","width":160,"height":160,"x":205,"y":30},{"id":"room_2","text":"廁所","color":"#F6EFBC","width":110,"height":150,"x":70,"y":230},{"id":"room_3","text":"廚房","color":"#F4D6AE","width":160,"height":80,"x":380,"y":200},{"id":"room_4","text":"妹妹房間","color":"#B7E2C1","width":230,"height":85,"x":285,"y":405},{"id":"room_5","text":"客廳","color":"#F7D8A8","width":150,"height":230,"x":595,"y":115},{"id":"room_6","text":"大哥房間","color":"#F97A4E","width":230,"height":85,"x":520,"y":405}]}'
-floorPlanParser=function(json) {
-  var data = JSON.parse(json);
-  console.log(data);
-  return data;
-};
-data=floorPlanParser(init);
-var data_point=[];
-var walls = data.floorplan.walls;
-var corners = data.floorplan.corners;
+var choose_region;
+var prevDiv = null;
+$.getJSON("floorplan.json", function(json) {
+    data = json;
+    var data_point = [];
+    var walls = data.floorplan.walls;
+    var corners = data.floorplan.corners;
 
 
-var line = d3.svg.line()
-    .x(function(d) {
-      return d.x;
-    })
-    .y(function(d) {
-      return d.y;
-    })
-    .interpolate('linear-closed');
-var svg = d3.select('#floorplan')
-      .append('svg')
-      .attr({
-        'position': 'absolute',
-        'width':"100%",
-        'height':"100%"
-        //'background-color':'blue'
-      });
-data_point=[]
-for(var i=0;i<walls.length;++i){
-  data_point.push({x:corners[walls[i]["corner1"]].x/1.3+220,y:corners[walls[i]["corner1"]].y/1.3+650});
-  data_point.push({x:corners[walls[i]["corner2"]].x/1.3+220,y:corners[walls[i]["corner2"]].y/1.3+650});
-}
-svg.append('path')
-   .attr({
-     'position': 'absolute',
-     'd': line(data_point),
-     'y': 0,
-     'stroke': '#F7F6E4',
-     'stroke-width': '5px',
-     'fill': '#F7F6E4'
-   });
+    var line = d3.svg.line()
+        .x(function(d) {
+            return d.x;
+        })
+        .y(function(d) {
+            return d.y;
+        })
+        .interpolate('linear-closed');
+    var svg = d3.select('#floorplan')
+        .append('svg')
+        .attr({
+            'position': 'absolute',
+            'width': "100%",
+            'height': "100%"
+                //'background-color':'blue'
+        });
+    data_point = []
+    for (var i = 0; i < walls.length; ++i) {
+        data_point.push({ x: corners[walls[i]["corner1"]].x / 1.3 + 220, y: corners[walls[i]["corner1"]].y / 1.3 + 650 });
+        data_point.push({ x: corners[walls[i]["corner2"]].x / 1.3 + 220, y: corners[walls[i]["corner2"]].y / 1.3 + 650 });
+    }
+    svg.append('path')
+        .attr({
+            'position': 'absolute',
+            'd': line(data_point),
+            'y': 0,
+            'stroke': '#F7F6E4',
+            'stroke-width': '5px',
+            'fill': '#F7F6E4'
+        });
 
-var rooms = data.rooms;
-for(var i=0;i<rooms.length;++i){
+    var rooms = data.rooms;
+    for (var i = 0; i < rooms.length; ++i) {
+        svg.append('rect')
+            .attr({
+                'id': rooms[i].id,
+                'fill': rooms[i].color,
+                'width': rooms[i].width,
+                'height': rooms[i].height,
+                'x': rooms[i].x + 100,
+                'y': rooms[i].y,
+                'rx': '5px'
+            });
+        svg.append('text')
+            .attr({
+                'x': rooms[i].x + 100 + 5,
+                'y': rooms[i].y + 20,
+                'fill': 'white'
+            }).style({
+                'font-size': '20px'
+
+            }).text(rooms[i].text);
+    }
+    var furnish = data.items;
+    for (var i = 0; i < furnish.length; ++i) {
+        svg.append('image')
+            .attr({
+                'id': furnish[i].item_name,
+                'position': 'absolute',
+                'href': furnish[i].model_url,
+                'x': furnish[i].xpos + 100,
+                'y': furnish[i].ypos,
+            });
+    }
 
 
- svg.append('rect')
-    .attr({
-      'id':rooms[i].id,
-      'fill':rooms[i].color,
-      'width':rooms[i].width,
-      'height':rooms[i].height,
-      'x': rooms[i].x+100,
-      'y': rooms[i].y
-    });
-  svg.append('text')
-   .attr({
-     'x':rooms[i].x+100+5,
-     'y':rooms[i].y+20,
-     'fill':'white'
-   }).style({
-     'font-size':'20px'
-
-   }).text(rooms[i].text);
-}
-var furnish = data.items;
-for(var i=0;i<furnish.length;++i){
- svg.append('image')
-    .attr({
-      'id':furnish[i].item_name,
-      'position': 'absolute',
-      'href':furnish[i].model_url,
-      'x': furnish[i].xpos+100,
-      'y': furnish[i].ypos,
-    });
-}
+    for (var i = 0; i < rooms.length; i++) {
+        var temp = document.getElementById("room_" + (i + 1));
+        temp.classList.add('floor-item');
+        temp.addEventListener('click', function(e) {
+            choose_region = e.target.id;
+            if (prevDiv != null && prevDiv[0].id == e.target.id) {
+                prevDiv = null;
+                $(this).removeClass('border');
+                choose_region = null;
+            } else {
+                $(prevDiv).removeClass('border');
+                $(this).addClass('border');
+                prevDiv = $(this);
+            }
+        })
+    }
+});
