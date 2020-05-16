f926d30d3bcbdc38f6403cf309f7c67b2882e4e7
robertlin0401 <robertlin0401@gmail.com>
2020-05-16 22:49:44 +0800

    Refactor: floorplan

--- client/public/src/js/test.js
+++ client/public/src/js/test.js
@@ -1,6 +1,5 @@
 let mode = "line";
-let switchmode = document.getElementById("button");
-
+let switchmode = document.getElementById("switch");
 switchmode.addEventListener('click', () => {
 	if (mode === "line") {
 		mode = "rect";
@@ -9,8 +8,8 @@ switchmode.addEventListener('click', () => {
 		mode = "line";
 		document.getElementById("color_canvas").style.visibility = "hidden";
 	}
-	console.log("switch mode to " + mode);
 });
+document.getElementById("undo").addEventListener('click', undo);
 
 let panel = {
     width: 1000,
@@ -23,72 +22,60 @@ let room_counter=0;
 let corners = [];
 let walls = [];
 let previewedWall = [];
+let wall_stack = [];
 var room_stack = [];
 let nowCorner = {
 	id: "",
 	x: 0,
 	y: 0
 };
-let undo = document.getElementById("undo");
-undo.addEventListener('click', () => {
-	let class_name=room_stack.pop()
-	console.log("undo",class_name);
-	if(class_name===undefined){
-		console.log("Undo all");
-	}
-	else{
-		d3.selectAll("."+class_name).attr("class","square").style("fill", "#ffffff");
-		room_counter-=1;
-	}
-});
 
 function drawline(d, isDot, isLine) {
 
 	document.getElementsByTagName("svg")[0].removeEventListener('mousemove', previewWall);
 
+	// calculate dot position
 	let x, y;
 	if (isDot) {
 		x = d.x;
 		y = d.y;
 	} else if (isLine) {
-		x = (parseInt(d.corner2.x / gridSize) + ((d.corner2.x % gridSize) > (gridSize / 2))) * gridSize + 1;
-		y = (parseInt(d.corner2.y / gridSize) + ((d.corner2.y % gridSize) > (gridSize / 2))) * gridSize + 1;
+		x = (Math.floor(d.corner2.x / gridSize) + ((d.corner2.x % gridSize) > (gridSize / 2))) * gridSize + 1;
+		y = (Math.floor(d.corner2.y / gridSize) + ((d.corner2.y % gridSize) > (gridSize / 2))) * gridSize + 1;
 	} else {
-		x = ((event.clientX % gridSize) > (gridSize / 2) || (event.clientX % gridSize) <= 0) * gridSize + d.x;
-		y = ((event.clientY % gridSize) > (gridSize / 2) || (event.clientY % gridSize) <= 0) * gridSize + d.y;
-		if (event.clientX - d.x == gridSize) x = event.clientX;
-		if (event.clientY - d.y == gridSize) y = event.clientY;
+		x = ((event.layerX % gridSize) > (gridSize / 2) || (event.layerX % gridSize) <= 0) * gridSize + d.x;
+		y = ((event.layerY % gridSize) > (gridSize / 2) || (event.layerY % gridSize) <= 0) * gridSize + d.y;
+		if (event.layerX - d.x == gridSize) x = event.layerX;
+		if (event.layerY - d.y == gridSize) y = event.layerY;
 	}
 	let newCorner = {
 		id: Math.random().toString(36).slice(-8),
 		x: x,
 		y: y,
-		r: 5
+		r: 6
 	};
 
+	// update dots
 	corners = corners.map(corner => {
 		if (corner.x == newCorner.x && corner.y == newCorner.y) {
 			newCorner.id = corner.id;
 			return false;
 		} else {
-			corner.r = 3;
+			corner.r = 4;
 			return corner;
 		}
 	}).filter(ele => {
 		return ele;
 	});
 	if (nowCorner.x == newCorner.x && nowCorner.y == newCorner.y) {
-		newCorner.r = 3;
+		newCorner.r = 4;
 		nowCorner.id = "";
 		nowCorner.x = 0;
 		nowCorner.y = 0;
-		previewedWall = [];
-		d3.select("#previewWall")
-			.selectAll(".wall")
-			.data(previewedWall)
-			.exit().remove();
 	} else {
 		document.getElementsByTagName("svg")[0].addEventListener('mousemove', previewWall);
+
+		// calculate line position
 		if (!(nowCorner.x == 0 && nowCorner.y == 0)) {
 			let newWall = {
 				corner1: previewedWall[0].corner1,
@@ -109,84 +96,115 @@ function drawline(d, isDot, isLine) {
 			});
 			walls.push(Object.assign({}, newWall));
 		}
+
 		nowCorner.id = newCorner.id;
 		nowCorner.x = newCorner.x;
 		nowCorner.y = newCorner.y;
 	}
 	corners.push(newCorner);
 
-	d3.select("#corners")
-		.selectAll(".corner")
-		.data(corners)
-		.enter().append("circle")
-		.attr("class", "corner")
-		.attr("cx", (d) => { return d.x; })
-		.attr("cy", (d) => { return d.y; })
-		.attr("r", (d) => { return d.r; })
-		.attr("stroke-width", 0)
-		.attr("fill", "blue")
-		.on('click', (d) => { drawline(d, true, false); });
-	d3.select("#corners")
-		.selectAll("circle")
-		.data(corners)
-		.attr("cx", (d) => { return d.x; })
-		.attr("cy", (d) => { return d.y; })
-		.attr("r", (d) => { return d.r; })
-		.exit().remove();
-
-	d3.select("#walls")
-		.selectAll(".wall")
-		.data(walls)
-		.enter().append("line")
-		.attr("class", "wall")
-		.attr("x1", (d) => { console.log(d); return d.corner1.x; })
-		.attr("y1", (d) => { return d.corner1.y; })
-		.attr("x2", (d) => { return d.corner2.x; })
-		.attr("y2", (d) => { return d.corner2.y; })
-		.attr("stroke", "blue")
-		.attr("stroke-width", 2);
-
+	render("line");
+	previewWall(event);
+	
 }
 
-function previewWall() {
-	previewedWall = [{
-		corner1: Object.assign({}, nowCorner),
-		corner2: {
-			x: event.clientX,
-			y: event.clientY
-		}
-	}];
-	d3.select("#previewWall")
-		.selectAll(".wall")
-		.data(previewedWall)
-		.enter().append("line")
-		.attr("class", "wall")
-		.attr("x1", (d) => { return d.corner1.x; })
-		.attr("y1", (d) => { return d.corner1.y; })
-		.attr("x2", (d) => { return d.corner2.x; })
-		.attr("y2", (d) => { return d.corner2.y; })
-		.attr("stroke", "blue")
-		.attr("stroke-width", 2)
-		.style("opacity", 0.5)
-		.on('click', (d) => { drawline(d, false, true); });
-	d3.select("#previewWall")
-		.selectAll(".wall")
-		.data(previewedWall)
-		.attr("x1", (d) => { return d.corner1.x; })
-		.attr("y1", (d) => { return d.corner1.y; })
-		.attr("x2", (d) => { return d.corner2.x; })
-		.attr("y2", (d) => { return d.corner2.y; })
-		.exit().remove();
+function previewWall(event) {
+	if (nowCorner.x == 0 && nowCorner.y == 0) {
+		previewedWall = [];
+	} else {
+		previewedWall = [{
+			corner1: Object.assign({}, nowCorner),
+			corner2: {
+				x: event.layerX,
+				y: event.layerY
+			}
+		}];
+	}
+	render("previewedWall");
 }
 
 function drawrect(d) {
-	console.log(d);
+	d3.selectAll(".square").style("fill", "#f0f0f0").attr("class","square")	// reset the class
+	let color = document.getElementById("color_canvas").value;
+	for (let i = Math.min(d.x, pre_x); i <= Math.max(d.x, pre_x) ; i+=gridSize) {
+		for (let j = Math.min(d.y, pre_y); j <= Math.max(d.y, pre_y); j+=gridSize) {
+			d3.select("#"+'_'+i.toString(10)+'_'+j.toString(10))
+				.style("fill", "#"+color)
+				.classed('choosing', true)	// add class to recognize the chosen one
+		}
+	}
 }
 
-function mousemove() {
-	var m = d3.mouse(this);
+function render(type) {
+	if (type === "line") {
+		d3.select("#corners")
+			.selectAll(".corner")
+			.data(corners)
+			.enter().append("circle")
+			.attr("class", "corner")
+			.attr("cx", (d) => { return d.x; })
+			.attr("cy", (d) => { return d.y; })
+			.attr("r", (d) => { return d.r; })
+			.attr("stroke-width", 0)
+			.attr("fill", "blue")
+			.on('click', (d) => { drawline(d, true, false); });
+		d3.select("#corners")
+			.selectAll("circle")
+			.data(corners)
+			.attr("cx", (d) => { return d.x; })
+			.attr("cy", (d) => { return d.y; })
+			.attr("r", (d) => { return d.r; })
+			.exit().remove();
+
+		d3.select("#walls")
+			.selectAll(".wall")
+			.data(walls)
+			.enter().append("line")
+			.attr("class", "wall")
+			.attr("x1", (d) => { return d.corner1.x; })
+			.attr("y1", (d) => { return d.corner1.y; })
+			.attr("x2", (d) => { return d.corner2.x; })
+			.attr("y2", (d) => { return d.corner2.y; })
+			.attr("stroke", "blue")
+			.attr("stroke-width", 2);
+	}
+	if (type === "previewedWall") {
+		d3.select("#previewWall")
+			.selectAll(".wall")
+			.data(previewedWall)
+			.enter().append("line")
+			.attr("class", "wall")
+			.attr("x1", (d) => { return d.corner1.x; })
+			.attr("y1", (d) => { return d.corner1.y; })
+			.attr("x2", (d) => { return d.corner2.x; })
+			.attr("y2", (d) => { return d.corner2.y; })
+			.attr("stroke", "blue")
+			.attr("stroke-width", 2)
+			.style("opacity", 0.5)
+			.on('click', (d) => { drawline(d, false, true); });
+		d3.select("#previewWall")
+			.selectAll(".wall")
+			.data(previewedWall)
+			.attr("x1", (d) => { return d.corner1.x; })
+			.attr("y1", (d) => { return d.corner1.y; })
+			.attr("x2", (d) => { return d.corner2.x; })
+			.attr("y2", (d) => { return d.corner2.y; })
+			.exit().remove();
+	}
 }
 
+function undo() {
+	if (mode === "line") {
+		// TODO
+	}
+	if (mode === "rect") {
+		let class_name=room_stack.pop()
+		if(class_name) {
+			d3.selectAll("."+class_name).attr("class","square").style("fill", "#f0f0f0");
+			room_counter-=1;
+		}
+	}
+}
 
 
 
@@ -233,21 +251,7 @@ function mousemove() {
 		.data(gridData)
 		.enter().append("g")
 		.attr("class", "row");
-	function onMouseMove(d){
-	  row.selectAll(".square").style("fill", "#fff").attr('class',"square")//reset the class
-	  a=Math.max(d.x,pre_x)
-	  b=Math.max(d.y,pre_y)
-		let color=document.getElementById("color_canvas").value;
-	  for (var i = Math.min(d.x,pre_x); i <= a ; i+=gridSize){
-	    for (var j = Math.min(d.y,pre_y); j <= b; j+=gridSize){
-	      //console.log(pre_x,pre_y)
-	      d3.select("#"+'_'+i.toString(10)+'_'+j.toString(10))
-	       .style("fill", color)
-	       .classed('choosing',true)//add class to recognize the chosen one
-	    }
-	  }
-
-	}
+	
 	var column = row.selectAll(".square")
 		.data(function(d) { return d; })
 		.enter().append("rect")
@@ -256,37 +260,32 @@ function mousemove() {
 		.attr("y", function(d) { return d.y; })
 		.attr("width", function(d) { return d.width; })
 		.attr("height", function(d) { return d.height; })
-		.attr("id",function(d){return '_'+d.x.toString(10)+'_'+d.y.toString(10);})
+		.attr("id", function(d) { return '_'+d.x.toString(10)+'_'+d.y.toString(10); })
 		.style("fill", "#f0f0f0")
 		.style("stroke", "#222")
 		.style("stroke-width", 0.3)
 		.on('click', function(d) {
 			if (mode === "line") {
 				drawline(d, false, false);
-			} else {
-				drawrect(d);
 			}
 		}).on('mousedown', function(d) {
 			if (mode === "rect"){
 				var m = d3.mouse(this);
-		    pre_x=Math.floor(m[0]/gridSize)*gridSize+1;
-		    pre_y=Math.floor(m[1]/gridSize)*gridSize+1;
-		    column.on("mousemove", onMouseMove);
+				pre_x=Math.floor(m[0]/gridSize)*gridSize+1;
+				pre_y=Math.floor(m[1]/gridSize)*gridSize+1;
+				column.on('mousemove', drawrect);
 			}
-
-	   }).on('mouseup',function(d){
-			 if (mode === "rect"){
-				 room_counter+=1
-		     column.on("mousemove", null);
-		     row.selectAll(".choosing")
-		      //.attr("id",function(d){return "chosen_"+Math.random().toString(36).slice(-8)})
-		      .attr("class","room_"+room_counter.toString(10))
-		      .classed('chosen',true)
-
+	   	})
+		.on('mouseup',function(d){
+			if (mode === "rect"){
+				console.log("end")
+				room_counter+=1;
+				column.on('mousemove', null);
+				row.selectAll(".choosing")
+					.attr("class","room_"+room_counter.toString(10))
+					.classed('chosen',true)
 				room_stack.push("room_"+room_counter.toString(10));
-			 }
-		}).on('dbclick', () => {
-			console.log("db");
+			}
 		});
 
 	grid.append("g").attr("id", "corners");
