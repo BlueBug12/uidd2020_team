e1433834e3e36d5e17001edcb89ed116a2412ece
BlueBug12 <benson.liu.himself@gmail.com>
2020-05-16 18:37:17 +0800

    add room divider

--- client/public/src/js/test.js
+++ client/public/src/js/test.js
@@ -1,10 +1,13 @@
 let mode = "line";
 let switchmode = document.getElementById("button");
+
 switchmode.addEventListener('click', () => {
 	if (mode === "line") {
 		mode = "rect";
+		document.getElementById("color_canvas").style.visibility = "visible";
 	} else {
 		mode = "line";
+		document.getElementById("color_canvas").style.visibility = "hidden";
 	}
 	console.log("switch mode to " + mode);
 });
@@ -14,15 +17,30 @@ let panel = {
     height: 600
 };
 let gridSize = 20;
-
+let pre_x=1;
+let pre_y=1;
+let room_counter=0;
 let corners = [];
 let walls = [];
 let previewedWall = [];
+var room_stack = [];
 let nowCorner = {
 	id: "",
 	x: 0,
 	y: 0
 };
+let undo = document.getElementById("undo");
+undo.addEventListener('click', () => {
+	let class_name=room_stack.pop()
+	console.log("undo",class_name);
+	if(class_name===undefined){
+		console.log("Undo all");
+	}
+	else{
+		d3.selectAll("."+class_name).attr("class","square").style("fill", "#ffffff");
+		room_counter-=1;
+	}
+});
 
 function drawline(d, isDot, isLine) {
 
@@ -165,7 +183,9 @@ function drawrect(d) {
 	console.log(d);
 }
 
-
+function mousemove() {
+	var m = d3.mouse(this);
+}
 
 
 
@@ -178,11 +198,11 @@ function drawrect(d) {
 		var ypos = 1;
 		var width = gridSize;
 		var height = gridSize;
-		
-		// iterate for rows	
+
+		// iterate for rows
 		for (var row = 0; row < panel.height / height; row++) {
 			data.push( new Array() );
-			
+
 			// iterate for cells/columns inside rows
 			for (var column = 0; column < panel.width / width; column++) {
 				data[row].push({
@@ -197,23 +217,37 @@ function drawrect(d) {
 			// reset the x position after a row is complete
 			xpos = 1;
 			// increment the y position for the next row. Move it down 50 (height variable)
-			ypos += height;	
+			ypos += height;
 		}
 		return data;
 	}
-	
+
 	var gridData = gridData();
-	
+
 	var grid = d3.select("#grid")
 		.append("svg")
 		.attr("width", panel.width+gridSize)
 		.attr("height", panel.height+gridSize);
-		
+
 	var row = grid.selectAll(".row")
 		.data(gridData)
 		.enter().append("g")
 		.attr("class", "row");
-		
+	function onMouseMove(d){
+	  row.selectAll(".square").style("fill", "#fff").attr('class',"square")//reset the class
+	  a=Math.max(d.x,pre_x)
+	  b=Math.max(d.y,pre_y)
+		let color=document.getElementById("color_canvas").value;
+	  for (var i = Math.min(d.x,pre_x); i <= a ; i+=gridSize){
+	    for (var j = Math.min(d.y,pre_y); j <= b; j+=gridSize){
+	      //console.log(pre_x,pre_y)
+	      d3.select("#"+'_'+i.toString(10)+'_'+j.toString(10))
+	       .style("fill", color)
+	       .classed('choosing',true)//add class to recognize the chosen one
+	    }
+	  }
+
+	}
 	var column = row.selectAll(".square")
 		.data(function(d) { return d; })
 		.enter().append("rect")
@@ -222,6 +256,7 @@ function drawrect(d) {
 		.attr("y", function(d) { return d.y; })
 		.attr("width", function(d) { return d.width; })
 		.attr("height", function(d) { return d.height; })
+		.attr("id",function(d){return '_'+d.x.toString(10)+'_'+d.y.toString(10);})
 		.style("fill", "#f0f0f0")
 		.style("stroke", "#222")
 		.style("stroke-width", 0.3)
@@ -231,8 +266,26 @@ function drawrect(d) {
 			} else {
 				drawrect(d);
 			}
-		})
-		.on('dbclick', () => {
+		}).on('mousedown', function(d) {
+			if (mode === "rect"){
+				var m = d3.mouse(this);
+		    pre_x=Math.floor(m[0]/gridSize)*gridSize+1;
+		    pre_y=Math.floor(m[1]/gridSize)*gridSize+1;
+		    column.on("mousemove", onMouseMove);
+			}
+
+	   }).on('mouseup',function(d){
+			 if (mode === "rect"){
+				 room_counter+=1
+		     column.on("mousemove", null);
+		     row.selectAll(".choosing")
+		      //.attr("id",function(d){return "chosen_"+Math.random().toString(36).slice(-8)})
+		      .attr("class","room_"+room_counter.toString(10))
+		      .classed('chosen',true)
+
+				room_stack.push("room_"+room_counter.toString(10));
+			 }
+		}).on('dbclick', () => {
 			console.log("db");
 		});
 
