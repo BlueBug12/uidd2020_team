daf9fe2336a9b44e1883640bc3d82b7cdd602cd8
BlueBug <benson.liu.himself@gmail.com>
2020-07-02 01:52:33 +0800

    connect to backend

--- client/public/src/js/monster.js
+++ client/public/src/js/monster.js
@@ -1,19 +1,54 @@
-var rh;
-var lh;
-var rl;
-var ll;
+//const { set } = require("mongoose");
 
+var rh=0;
+var lh=0;
+var rl=0;
+var ll=0;
+$(document).on('mouseenter', '#UserImg', function () {
+    $(this).css('border-color','#D0D9DC');
+    }).on('mouseleave', '#UserImg', function () {
+    $(this).css('border-color','#789FB3');
+    });
+  let span_menu=0;
+$(document).on('click', '#UserImg', function () {
+    if(!span_menu){
+        $('.menubar').css('visibility','visible');
+        span_menu=1;
+    }
+    else{
+        $('.menubar').css('visibility','hidden');
+        span_menu=0;
+    }
+});
+//click region except menubar
+$(document).mouseup(function(e){
+    var _con = $('.menubar'); 
+    if(!_con.is(e.target) && _con.has(e.target).length === 0){ 
+      $('.menubar').css('visibility','hidden');
+      span_menu=0;
+    }
+  });
+$(document).on('mouseenter', '.menubar', function () {
+    $(this).css('background','#d6dde4');
+    }).on('mouseleave', '.menubar', function () {
+    $(this).css('background','#b8bec4');
+    });
 $(document).on('click',"#bar2",function(){
     localStorage.clear();
 })
-
-$(document).on('click',"#left_hand_button",function(){
-    
-    let v = lh%10
-    if(lh!=20)
-        $("#left_hand_value").css('width',`${42+25*v}px`);
+function store(){
+    $.post('./users/updatemonsterstate', {
+        account: localStorage.account,
+        monster:[lh,rh,ll,rl]
+    }, (res) => {
+    });
+}
+function set_lh(v){
+    if(v!=20)
+        $("#left_hand_value").css('width',`${42+25*(v%10)}px`);
     else
         $("#left_hand_value").css('width',`284px`);
+
     if(v==1){
         $('#left_hand').css('right','376px').css('top',"312px").css('width','46px').css('height','46px');
     }else if(v==2){
@@ -32,30 +67,43 @@ $(document).on('click',"#left_hand_button",function(){
         $('#left_hand').css('right','346px').css('top',"303px").css('width','88px').css('height','88px');;
     }else if(v==9){
         $('#left_hand').css('right','342px').css('top',"302px").css('width','94px').css('height','94px');
+    }else if(v==10){
+        $('#left_hand').css('right','373px').css('top',"282px").css('width','40px').css('height','40px');
+        document.getElementById('lh').src='./img/monster/lefthand2.svg';
+        document.getElementById("lh_level").innerHTML="Lv.2";
+    }else if(v==11){
+        $('#left_hand').css('right','368px').css('top',"277px").css('width','46px').css('height','46px');
+    }else if(v==12){
+        $('#left_hand').css('right','362px').css('top',"272px").css('width','52px').css('height','52px');
+    }else if(v==13){
+        $('#left_hand').css('right','356px').css('top',"266px").css('width','58px').css('height','58px');
+    }else if(v==14){
+        $('#left_hand').css('right','350px').css('top',"261px").css('width','64px').css('height','64px');
+    }else if(v==15){
+        $('#left_hand').css('right','344px').css('top',"255px").css('width','70px').css('height','70px');
+    }else if(v==16){
+        $('#left_hand').css('right','338px').css('top',"250px").css('width','76px').css('height','76px');
+    }else if(v==17){
+        $('#left_hand').css('right','332px').css('top',"244px").css('width','82px').css('height','82px');
+    }else if(v==18){
+        $('#left_hand').css('right','326px').css('top',"238px").css('width','88px').css('height','88px');
+    }else if(v==19){
+        $('#left_hand').css('right','320px').css('top',"230px").css('width','94px').css('height','94px');
     }else{
-        if(lh<=19){
-            lh+=1;
-            document.getElementById('lh').src='./img/monster/lefthand2.svg';
-            $('#left_hand').css('width','40px').css('height','40px');
-            document.getElementById("lh_level").innerHTML="Lv.2";
-        }
-        else{
-            lh-=1
-        }
-        
-    }
-    lh+=1;
-    $.post('./users/updatemonsterstate', {
-        account: localStorage.account,
-        monster:[lh,rh,ll,rl]
-    }, (res) => {
-    });
+        $('#left_hand').css('right','316px').css('top',"225px").css('width','100px').css('height','100px');
+    }   
     
-})
+}
+
+  /* 
 $(document).on('click',"#right_hand_button",function(){
     let v = rh%10;
     if(rh!=20)
         $("#right_hand_value").css('width',`${42+25*v}px`);
+})*/
+function set_rh(v){
+    if(v!=20)
+        $("#right_hand_value").css('width',`${42+25*(v%10)}px`);
     else
         $("#right_hand_value").css('width',`284px`);
     if(v==1){
@@ -76,30 +124,39 @@ $(document).on('click',"#right_hand_button",function(){
         $('#right_hand').css('right','521px').css('top',"308px").css('width','88px').css('height','88px');;
     }else if(v==9){
         $('#right_hand').css('right','518px').css('top',"306px").css('width','94px').css('height','94px');
+    }else if(v==10){
+        $('#right_hand').css('right','558px').css('top',"289px").css('width','40px').css('height','40px');
+        document.getElementById('rh').src='./img/monster/righthand2.svg';
+        document.getElementById("rh_level").innerHTML="Lv.2";
+    }else if(v==11){
+        $('#right_hand').css('right','558px').css('top',"284px").css('width','46px').css('height','46px');
+    }else if(v==12){
+        $('#right_hand').css('right','558px').css('top',"278px").css('width','52px').css('height','52px');
+    }else if(v==13){
+        $('#right_hand').css('right','558px').css('top',"274px").css('width','58px').css('height','58px');
+    }else if(v==14){
+        $('#right_hand').css('right','552px').css('top',"271px").css('width','64px').css('height','64px');
+    }else if(v==15){
+        $('#right_hand').css('right','552px').css('top',"268px").css('width','70px').css('height','70px');
+    }else if(v==16){
+        $('#right_hand').css('right','552px').css('top',"260px").css('width','76px').css('height','76px');
+    }else if(v==17){
+        $('#right_hand').css('right','552px').css('top',"255px").css('width','82px').css('height','82px');
+    }else if(v==18){
+        $('#right_hand').css('right','550px').css('top',"250px").css('width','88px').css('height','88px');
+    }else if(v==19){
+        $('#right_hand').css('right','549px').css('top',"244px").css('width','94px').css('height','94px');
     }else{
-        if(rh<=19){
-            rh+=1;
-            document.getElementById('rh').src='./img/monster/righthand2.svg';
-            $('#right_hand').css('width','40px').css('height','40px');
-            document.getElementById("rh_level").innerHTML="Lv.2";
-        }
-        else{
-            rh-=1;
-        }
+        $('#right_hand').css('right','546px').css('top',"242px").css('width','100px').css('height','100px');
     }
-    rh+=1;
-    $.post('./users/updatemonsterstate', {
-        account: localStorage.account,
-        monster:[lh,rh,ll,rl]
-    }, (res) => {
-    });
-})
-$(document).on('click',"#left_leg_button",function(){
-    let v = ll%10;
+    
+}
+
+function set_ll(v){
     if(ll!=20)
-        $("#left_leg_value").css('width',`${42+25*v}px`);
+        $("#left_leg_value").css('width',`${42+25*(v%10)}px`);
     else
-    $("#left_leg_value").css('width',`284px`);
+        $("#left_leg_value").css('width',`284px`);
     if(v==1){
         $('#left_leg').css('right','406px').css('top',"427px").css('width','46px').css('height','46px');
     }else if(v==2){
@@ -118,29 +175,36 @@ $(document).on('click',"#left_leg_button",function(){
         $('#left_leg').css('right','382px').css('top',"422px").css('width','88px').css('height','88px');;
     }else if(v==9){
         $('#left_leg').css('right','380px').css('top',"424px").css('width','94px').css('height','94px');
+    }else if(v==10){
+        $('#left_leg').css('right','400px').css('top',"430px").css('width','40px').css('height','40px');
+        document.getElementById('ll').src='./img/monster/leftleg2.svg';
+        document.getElementById("ll_level").innerHTML="Lv.2";
+    }else if(v==11){
+        $('#left_leg').css('right','396px').css('top',"428px").css('width','46px').css('height','46px');
+    }else if(v==12){
+        $('#left_leg').css('right','390px').css('top',"428px").css('width','52px').css('height','52px');
+    }else if(v==13){
+        $('#left_leg').css('right','386px').css('top',"428px").css('width','58px').css('height','58px');
+    }else if(v==14){
+        $('#left_leg').css('right','380px').css('top',"426px").css('width','64px').css('height','64px');
+    }else if(v==15){
+        $('#left_leg').css('right','375px').css('top',"426px").css('width','70px').css('height','70px');
+    }else if(v==16){
+        $('#left_leg').css('right','370px').css('top',"426px").css('width','76px').css('height','76px');
+    }else if(v==17){
+        $('#left_leg').css('right','364px').css('top',"424px").css('width','82px').css('height','82px');
+    }else if(v==18){
+        $('#left_leg').css('right','360px').css('top',"426px").css('width','88px').css('height','88px');
+    }else if(v==19){
+        $('#left_leg').css('right','355px').css('top',"426px").css('width','94px').css('height','94px');
     }else{
-        if(ll<=19){
-            ll+=1;
-            document.getElementById('ll').src='./img/monster/leftleg2.svg';
-            $('#left_leg').css('width','40px').css('height','40px');
-            document.getElementById("ll_level").innerHTML="Lv.2";
-        }
-        else{
-            ll-=1;
-        }
-        
+        $('#left_leg').css('right','350px').css('top',"426px").css('width','100px').css('height','100px');
     }
-    ll+=1;
-    $.post('./users/updatemonsterstate', {
-        account: localStorage.account,
-        monster:[lh,rh,ll,rl]
-    }, (res) => {
-    });
-})
-$(document).on('click',"#right_leg_button",function(){
-    let v = rl%10;
+    
+}
+function set_rl(v){
     if(rl!=20)
-        $("#right_leg_value").css('width',`${42+25*v}px`);
+        $("#right_leg_value").css('width',`${42+25*(v%10)}px`);
     else
         $("#right_leg_value").css('width',`284px`);
     if(v==1){
@@ -161,24 +225,61 @@ $(document).on('click',"#right_leg_button",function(){
         $('#right_leg').css('right','510px').css('top',"422px").css('width','88px').css('height','88px');;
     }else if(v==9){
         $('#right_leg').css('right','506px').css('top',"423px").css('width','94px').css('height','94px');
+    }else if(v==10){
+        $('#right_leg').css('right','528px').css('top',"425px").css('width','40px').css('height','40px');
+        document.getElementById('rl').src='./img/monster/rightleg2.svg';
+        document.getElementById("rl_level").innerHTML="Lv.2";
+    }else if(v==11){
+        $('#right_leg').css('right','528px').css('top',"425px").css('width','46px').css('height','46px');
+    }else if(v==12){
+        $('#right_leg').css('right','524px').css('top',"425px").css('width','52px').css('height','52px');
+    }else if(v==13){
+        $('#right_leg').css('right','524px').css('top',"425px").css('width','58px').css('height','58px');
+    }else if(v==14){
+        $('#right_leg').css('right','522px').css('top',"425px").css('width','64px').css('height','64px');
+    }else if(v==15){
+        $('#right_leg').css('right','522px').css('top',"425px").css('width','70px').css('height','70px');
+    }else if(v==16){
+        $('#right_leg').css('right','518px').css('top',"425px").css('width','76px').css('height','76px');
+    }else if(v==17){
+        $('#right_leg').css('right','514px').css('top',"425px").css('width','82px').css('height','82px');
+    }else if(v==18){
+        $('#right_leg').css('right','512px').css('top',"425px").css('width','88px').css('height','88px');
+    }else if(v==19){
+        $('#right_leg').css('right','510px').css('top',"425px").css('width','94px').css('height','94px');
     }else{
-        if(rl<=19){
-            rl+=1;
-            document.getElementById('rl').src='./img/monster/rightleg2.svg';
-            $('#right_leg').css('width','40px').css('height','40px');
-            document.getElementById("rl_level").innerHTML="Lv.2";
-        }
-        else{
-            rl-=1;
-        }
-       
+        $('#right_leg').css('right','508px').css('top',"423px").css('width','100px').css('height','100px');
     }
-    rl+=1;
-    $.post('./users/updatemonsterstate', {
-        account: localStorage.account,
-        monster:[lh,rh,ll,rl]
-    }, (res) => {
-    });
+}
+$(document).on('click',"#left_hand_button",function(){
+    if(lh<20){
+        lh+=1;
+        set_lh(lh);
+        document.getElementById("points").innerHTML="test";
+        store();
+    }
+    
+})
+$(document).on('click',"#right_hand_button",function(){
+    if(rh<20){
+        rh+=1;
+        set_rh(rh);
+        store();
+    }
+})
+$(document).on('click',"#left_leg_button",function(){
+    if(ll<20){
+        ll+=1;
+        set_ll(ll);
+        store();
+    }
+})
+$(document).on('click',"#right_leg_button",function(){
+    if(rl<20){
+        rl+=1;
+        set_rl(rl);
+        store();
+    }
 })
 
 
--- server/index.js
+++ server/index.js
@@ -17,11 +17,8 @@ const userRoute = require('./routes/Users');
 app.use('/tasks',taskRoute);
 app.use('/users',userRoute);
 
-<<<<<<< HEAD
-const port = 1118;
-=======
-const port = 2222;
->>>>>>> add animation
+const port = 4444;
+
 https.createServer(SERVER_CONFIG, app)
      .listen(port,function() { console.log("HTTPS sever started"); }
 );
